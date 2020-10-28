c5dd80d3a0f0f9de98c39b58ecafa8b23b7ff3b4
isFernandes <iago.stosfernandes@gmail.com>
2020-10-27 23:12:56 -0300

    Icon and landing page start

---
+++ frontend/src/pages/Landing/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+function Landing() {
+    return(
+        <Container >
+            ACONTECE ALGO AQUI
+        </Container>
+    );
+}
+
+export default Landing;
+
+const Container = styled.div`
+  background-color: crimson;
+  flex: 1;
+`;
