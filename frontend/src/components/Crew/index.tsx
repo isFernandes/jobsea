01fe7318392378f2fdec3d98e36d75b2e9e8871b
isFernandes <iago.stosfernandes@gmail.com>
2020-11-05 12:29:09 -0300

    pagina de cadastro e estrutura componente crew

---
+++ frontend/src/components/Crew/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import './index.css';
+
+const Crew: React.FC = () => {
+  return(
+    <Container>
+        <CrewRanting>
+
+        </CrewRanting>
+        <CrewInfo>
+
+        </CrewInfo>
+        <CrewValue>
+
+        </CrewValue>
+    </Container>
+  );
+}
+
+export default Crew;
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-co
+`;
+
+const CrewRanting = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-co
+`;
+
+const CrewInfo = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-co
+`;
+
+const CrewValue = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-co
+`;
+
