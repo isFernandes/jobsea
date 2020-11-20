ab2b318157cfcdaa99ee578e47ebf0e2954536c7
Rogerio-Viana <vrogerio07@gmail.com>
2020-11-20 17:07:41 -0300

    config spring backend

---
+++ backend/src/test/java/br/com/jobsea/JobseaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jobsea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobseaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
